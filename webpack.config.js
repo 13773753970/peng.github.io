7d6115671c6c8bd7ddf42df446df892ad48dfb44
Daniel <zhoupeng@zhoupengdeMac-mini.local>
2022-02-28 11:41:43 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
-    static: './dist',
+    static: './',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -15,6 +15,6 @@ module.exports = {
   ],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '/'),
   },
 };
