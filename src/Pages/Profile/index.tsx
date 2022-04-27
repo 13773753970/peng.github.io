8e537730eae5cb705577dcc55827ca8bb69981f5
Daniel <zhoupeng@zhoupengdeMac-mini.local>
2022-04-27 22:43:19 +0800

    test share

--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -16,6 +16,15 @@ function ProfilePage(props: Props) {
     const person = people.find(x => x.id === params.id)
     const contactUsRef = useRef<HTMLDivElement | null>(null)
     useEffect(() => window.scrollTo(0, 0), [])
+    // useEffect(() => {
+    //     (function(d, s, id) {
+    //         var js: any, fjs: any = d.getElementsByTagName(s)[0];
+    //         if (d.getElementById(id)) return;
+    //         js = d.createElement(s); js.id = id;
+    //         js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    //         fjs.parentNode.insertBefore(js, fjs);
+    //         }(document, 'script', 'facebook-jssdk'))
+    // }, [])
     const [regions, _setRegions] = useState<{title: string, domRef: React.MutableRefObject<HTMLDivElement | null>}[]>([])
     const setRegions = useCallback((regions: {title: string, domRef: React.MutableRefObject<HTMLDivElement | null>}[]) => {
         _setRegions(regions.concat([{title: 'Contact', domRef: contactUsRef}]))
@@ -37,6 +46,10 @@ function ProfilePage(props: Props) {
             </Helmet>
             <Header regions={regions}/>
             <Profile setRegions={setRegions}/>
+            {/* <div className="fb-share-button" 
+data-href="https://digital-transformation-lab.github.io/Arif-Ali-Khan" 
+data-layout="button_count">
+</div> */}
             <ContactUs forwardRef={contactUsRef}/>
         </>
     )
